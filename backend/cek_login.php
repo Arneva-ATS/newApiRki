8e1c35a4dd4a2af903113574dd9ad657549a1087
Muhammad Rifqi <ats.muhammadr@xl.co.id>
2023-09-06 22:35:59 +0700

    aaaa

---
+++ backend/cek_login.php
@@ -0,0 +1,34 @@
+<?php
+error_reporting(0);
+session_start();
+include"config.php";
+
+$un = htmlentities($_POST['username']);
+$pw = htmlentities($_POST['password']);
+
+if(!empty($un) OR !empty($pw)){
+
+$sql = mysqli_query($koneksi,"select * from pengguna where username = '".$un."' and password = '".md5($pw)."'");
+$data = mysqli_fetch_assoc($sql);
+$check = mysqli_num_rows($sql);
+    if($check > 0 ){
+
+        $_SESSION['id'] = $data['id'];
+        $_SESSION['username'] = $data['username'];
+        $_SESSION['status'] = $data['status'];
+        $_SESSION['id_koperasi'] = $data['id_koperasi'];
+        $_SESSION['token'] = $data['token'];
+
+        header('location:dashboard.php');
+
+    }else{
+        echo "<h3 align='center'> User Tidak Ada Di Database! </h3> <hr> <p align='center'> Ulangi Login <a href='index.php'> Ulangi </a></p>";
+    }
+
+}else{
+    echo "<h3 align='center'> Login Gagal </h3> <hr> <p align='center'> Ulangi Login <a href='index.php'> Ulangi </a></p>";
+}
+
+
+
+?>
