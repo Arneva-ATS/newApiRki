4c4e25470444c0c4ac90681d6e1b1619fe16a597
muhammad rifqi <muhammad45rifki@gmail.com>
2023-09-13 11:16:43 +0700

    aaaaa

--- backend/menu.php
+++ backend/menu.php
@@ -119,6 +119,20 @@
                                     </nav>
                                 </div>
 
+                                
+                                <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapsePemesanan" aria-expanded="false" aria-controls="collapsePemesanan">
+                                    <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
+                                        Penjualan
+                                    <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                                </a>
+                                <div class="collapse" id="collapsePemesanan" aria-labelledby="headingThree" data-bs-parent="#sidenavAccordion">
+                                    <nav class="sb-sidenav-menu-nested nav">
+                                        <a class="nav-link" href="?menu=pemesanan">Pemesanan</a>
+                                        <a class="nav-link" href="?menu=ppob"> PPOB </a>
+                                    </nav>
+                                </div>
+                                
+
                             </div>
                         </div>
 
