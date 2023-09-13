de3d7b09b103ea4391e1825276e43e4b46b14cbd
muhammad rifqi <muhammad45rifki@gmail.com>
2023-09-13 13:05:34 +0700

    aaaa

--- backend/include/pinjaman.php
+++ backend/include/pinjaman.php
@@ -56,7 +56,7 @@
                                             <td> 
                                                 <a href="aksi.php?act=approve_pinjaman&id=<?php echo $data['id'];?>" class="btn btn-warning" onclick="return confirm('Yakin Mau Di Approve ??')"> Approve </a> 
                                                 <a href="?menu=view_detail_pinjaman&id=<?php echo $data['id'];?>" class="btn btn-info"> Detail </a>     
-                                                <a href="aksi.php?act=hapus_pinjaman&id=<?php echo $data['id'];?>" class="btn btn-danger" onclick="return confirm('Yakin Mau Hapus ??')"> Hapus </a> `
+                                                <a href="aksi.php?act=hapus_pinjaman&id=<?php echo $data['id'];?>" class="btn btn-danger" onclick="return confirm('Yakin Mau Hapus ??')"> Hapus </a> 
                                             </td>
                                         </tr>
                                       <?php } ?>
