bc535a8b27e0447e560d2bbf3fb5588e1e4157d7
muhammad rifqi <muhammad45rifki@gmail.com>
2023-09-14 09:46:04 +0700

    qqqqqq

--- backend/include/toko.php
+++ backend/include/toko.php
@@ -36,7 +36,7 @@
                                     </tfoot>
                                     <tbody>
                                     <?php 
-                                        $sql = mysqli_query($koneksi,"select * from toko order by id desc");
+                                        $sql = mysqli_query($koneksi,"select * from toko where id_koperasi = '".$_SESSION['id_koperasi']."' order by id desc");
                                         while($data = mysqli_fetch_assoc($sql)){
                                     ?>
                                         <tr>
