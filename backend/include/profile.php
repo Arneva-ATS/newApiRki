bc52f6ef692d345bf6d79f79d62461dfdf834f01
muhammad rifqi <muhammad45rifki@gmail.com>
2023-09-14 09:14:05 +0700

    aaaaaa

--- backend/include/profile.php
+++ backend/include/profile.php
@@ -11,22 +11,22 @@
                             <div class="tab-content">
                                 <div class="tab-pane fade active show" id="account-general">
                                     <div class="card-body media align-items-start">
-                                        <img src="img/undraw_profile.svg" alt="" class="img-thumbnail" width="100">
+                                        <img src="assets/img/profile.JPG" alt="" class="img-thumbnail" width="100">
                                     </div>
                                     <hr class="border-light m-0">
 
                                     <div class="card-body">
                                         <div class="form-group">
                                             <label class="form-label">Username</label>
-                                            <input type="text" class="form-control mb-1" value="admin">
+                                            <input type="text" class="form-control mb-1" value="<?php echo $_SESSION['username']; ?>">
                                         </div>
                                         <div class="form-group">
                                             <label class="form-label">Name</label>
-                                            <input type="text" class="form-control" value="admin">
+                                            <input type="text" class="form-control" value="<?php echo $_SESSION['nama']; ?>">
                                         </div>
                                         <div class="form-group">
                                             <label class="form-label">Status User</label>
-                                            <input type="text" class="form-control" value="admin">
+                                            <input type="text" class="form-control" value="<?php echo $_SESSION['status']; ?>">
                                         </div>
                                     </div>
 
