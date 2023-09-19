d218e6edf1b2d6ee69e1646254c47dc34db916b7
muhammad rifqi <muhammad45rifki@gmail.com>
2023-09-19 13:56:57 +0700

    aaaaa

--- backend/include/barang.php
+++ backend/include/barang.php
@@ -84,7 +84,7 @@
                             $sql = mysqli_query($koneksi,"select * from pos where id_koperasi = '".$_SESSION['id_koperasi']."' order by id desc");
                             while($data = mysqli_fetch_assoc($sql)){
                             ?>
-                            <div class="col-lg-4 d-flex align-items-stretch">
+                            <div class="col-sm d-flex">
                             <div class="card card-body flex-fill" style="width: 12rem;">
                                 <img class="card-img-top" src="<?php echo $data['photo'];?>" alt="<?php echo $data['nama_barang'];?>">
                                 <div class="card-body">
@@ -97,7 +97,7 @@
                             </div>
                             <?php } ?>
                         </div>
-                        <br>
+                        
                     </div>
                 </main>
 
