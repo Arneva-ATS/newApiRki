50f309cea5202123181d4aaa2ab16ad0f355e8b8
muhammad rifqi <muhammad45rifki@gmail.com>
2023-09-19 14:38:41 +0700

    aaaa

--- backend/dashboard.php
+++ backend/dashboard.php
@@ -75,10 +75,9 @@ if(empty($_SESSION['username'])){
         <script src="js/datatables-simple-demo.js"></script>
         <script>
             function add(e){
-                // let idd = document.getElementById("nama_".e).value;
-                // let newIDD = parseInt(idd) + parseInt(1);
-                // alert(newIDD);
-                console.log(e)
+                let idd = document.getElementById("nama_".e).value;
+                let newIDD = parseInt(idd);
+                alert(newIDD);
             }
 
         </script>
