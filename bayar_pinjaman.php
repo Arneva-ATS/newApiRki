b813162ea0320d86cf94513ca5aff609114bd2a5
muhammad rifqi <muhammad45rifki@gmail.com>
2023-09-21 11:31:10 +0700

    aaaaaa

--- bayar_pinjaman.php
+++ bayar_pinjaman.php
@@ -33,7 +33,9 @@
                         $url = $url_live.'/'.$dir;
                         $sql2 = mysqli_query($koneksi,"insert into pembayaran_pinjaman(id_user,id_koperasi,id_pinjaman,jumlah_pinjaman,cicilan,no_rekening,keterangan,photo)values('".$data['id_user']."','3','".$data['id_pinjaman']."','".$data['jumlah_pinjaman']."','".$data['cicilan']."','".$data['no_rekening']."','".$data['keterangan']."','".$url."')");
                         if($sql2){
-                            mysqli_query($koneksi,"update pinjaman set sisa_pinjaman = sisa_pinjaman + '".$data['cicilan']."' where id = '".$data['id_pinjaman']."'");
+                            $idt = mysqli_insert_id($koneksi);
+                            $ddd  = mysqli_fetch_array(mysqli_query($koneksi,"select * from pembayaran_pinjaman where id = '".$idt."'"));
+                            mysqli_query($koneksi,"update pinjaman set sisa_pinjaman = sisa_pinjaman + '".$ddd['cicilan']."' where id = '".$ddd['id_pinjaman']."'");
                             echo json_encode(
                                 array(
                                     'response_code' => 200,
