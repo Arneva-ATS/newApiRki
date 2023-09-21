69eb72b54ba2dbe0e91b51300d04b0c17ce94c0a
muhammad rifqi <muhammad45rifki@gmail.com>
2023-09-21 11:45:16 +0700

    aaaaa

--- pinjaman.php
+++ pinjaman.php
@@ -14,15 +14,38 @@
             if($check > 0){
 
                 $data = json_decode(file_get_contents("php://input"),true);
-                $insert = mysqli_query($koneksi,"insert into pinjaman(id_user,jumlah_pinjaman,sisa_pinjaman,jenis_pinjaman,lama_angsuran,keterangan)values('".$row['id']."','".$data['jumlah_pinjaman']."','0','".$data['jenis_pinjaman']."','".$data['lama_angsuran']."','".$data['keterangan']."')");
-                if($insert){
-                    echo json_encode(
-                        array(
-                            'response_code' => 200,
-                            'message' => 'Success'
-                        )
-                    );
-                }
+
+                $cek_bayar = mysqli_fetch_array(mysqli_query($koneksi,"select * from pinjaman where id_user = '".$row['id']."' and status_lunas = 'belum_lunas'"));
+                    if($cek_bayar['jumlah_pinjaman'] <= $cek_bayar['sisa_pinjaman']){
+
+                        $insert = mysqli_query($koneksi,"insert into pinjaman(id_user,jumlah_pinjaman,jenis_pinjaman,lama_angsuran,keterangan)values('".$row['id']."','".$data['jumlah_pinjaman']."','".$data['jenis_pinjaman']."','".$data['lama_angsuran']."','".$data['keterangan']."')");
+                        if($insert){
+                            echo json_encode(
+                                array(
+                                    'response_code' => 200,
+                                    'message' => 'Success'
+                                )
+                            );
+                        }else{
+
+                            echo json_encode(
+                                array(
+                                    'response_code' => 401,
+                                    'message' => 'Failed'
+                                )
+                            );
+                        }
+
+                    }else{
+
+                        echo json_encode(
+                            array(
+                                'response_code' => 200,
+                                'message' => 'Pinjaman Sudah Selesai'
+                            )
+                        );
+                    }
+
                  	
             }else{
             echo json_encode(
