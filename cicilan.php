65a167a203e30906115256ea54152434ee08845f
muhammad rifqi <muhammad45rifki@gmail.com>
2023-09-22 12:42:10 +0700

    aaaa

---
+++ cicilan.php
@@ -0,0 +1,41 @@
+<?php
+
+        header("Access-Control-Allow-Origin: *");
+		header("Access-Control-Allow-Credentials: true");
+		header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+		header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
+        header("Content-Type: application/json; charset=utf-8");
+        
+        include "config.php";
+
+        $sql = mysqli_query($koneksi,"select * from pengguna where token = '".$_GET['token']."'");
+        $check = mysqli_num_rows($sql);
+            
+            if($check > 0){
+        
+                $sql2 = mysqli_query($koneksi,"select * from pembayaran_pinjaman where id_user = '".$rows['id']."' and id_pinjaman = '".$_GET['id_pinjaman']."'");
+                $count = mysqli_num_rows($sql2);
+                if($count > 0){
+                    $cc = $count;
+                }else{
+                    $cc = 1;
+                }
+
+                echo json_encode(
+                    array(
+                        'response_code' => 200,
+                        'data' => $cc
+                    )
+                );
+        	
+            }else{
+            echo json_encode(
+                    array(
+                        'response_code' => 401,
+                        'message' => 'Failed Generare Image!'
+                    )
+                );
+            }
+        
+                
+?>
