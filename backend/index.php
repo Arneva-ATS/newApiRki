024db54a40fc334a999c2dca9189095d465593f9
Dinoxxc <noorismawanandika@gmail.com>
2023-09-27 16:54:20 +0700

    Login

--- backend/index.php
+++ backend/index.php
@@ -1,47 +1,37 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <meta name="description" content="" />
-        <meta name="author" content="" />
-        <title>Login - Admin</title>
-        <link href="css/styles.css" rel="stylesheet" />
-        <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
-    </head>
-    <body class="bg-primary" style="background-image:url('assets/img/image29.png'); background-size:cover">
-        <div id="layoutAuthentication">
-            <div id="layoutAuthentication_content">
-                <main>
-                    <div class="container">
-                        <div class="row justify-content-center">
-                            <div class="col-lg-5">
-                                <div class="card shadow-lg border-0 rounded-lg mt-5">
-                                    <div class="card-header bg-info text-center"> <img src="assets/img/Logo.png" /> </div>
-                                    <div class="card-body">
-                                        <form method="POST" action="cek_login.php">
-                                            <div class="form-floating mb-3">
-                                                <input class="form-control" id="inputText" name="username" type="text" placeholder="username" />
-                                                <label for="inputText">Username</label>
-                                            </div>
-                                            <div class="form-floating mb-3">
-                                                <input class="form-control" id="inputPassword" name="password" type="password" placeholder="Password" />
-                                                <label for="inputPassword">Password</label>
-                                            </div>
-                                            <div class="d-flex align-items-center justify-content-between mt-4 mb-0">
-                                                <button class="btn btn-info" type="submit">Login</button>
-                                            </div>
-                                        </form>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </main>
-            </div>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Login</title>
+    <link rel="stylesheet" href="styles.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;500;600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+</head>
+<body>
+    <div class="row">
+        <div class="column">
+            <img class="logo" src="assets/img/Logo.png" />
+            <h1 class="welcoming">Welcome!</h1>
+            <h3 class="description">Control Data Center of Rumah Kesejahteraan Indonesia (RKI) The first step towards success is through this gateway. Together, we will forge new achievements and realize dreams. Enter your credentials and let's embark on a journey towards boundless progress and innovation.</h3>
+            <button onclick="window.location.href='https://rkicoop.co.id/'">Company Website</button>
         </div>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
-        <script src="js/scripts.js"></script>
-    </body>
+           
+        <div class="column">
+        <form method="POST" action="cek_login.php">
+                <h2 style="text-align: right"> Login to your Account</h2>
+                <hr class="right-align">
+
+                <label style="text-align: right" for="username">Username</label>
+                <input type="text" placeholder="User ID" id="username" name="username">
+
+                <label style="text-align: right" for="password">Password</label>
+                <input type="password" placeholder="Password" id="password" name="password">
+
+                <button type="submit">Log In</button>
+            </form>
+        </div>
+    </div>
+</body>
 </html>
